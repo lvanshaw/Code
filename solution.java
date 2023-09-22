c74144a046b52447a718274f81368d21a50d89ff
Syed Masood Divan Oli M <36790140+lvanshaw@users.noreply.github.com>
2023-09-22 11:57:11 +0530

    Update solution.java
    chess_moving, choloate_distribution, recursion sum_of_numbers, merge_arrays, merging_overlap_array

--- solution.java
+++ solution.java
@@ -1,4 +1,95 @@
 class solution{
+	public static void chess_moving(){
+		System.out.print("\nchess_moving: \n");
+		int d[] = {1,1}, s[] = {3,8};
+		int count = 0;
+		while(s[0] != d[0] || s[1] != d[1]){
+			if(s[0] < d[0] ){
+				s[0] = s[0]+1;
+				s[1] = s[1]+1;
+				count++;
+			}
+			else if( s[1] < d[1]){
+				s[1] = s[1]+1;
+				count++;
+			}
+			else if(s[0] > d[0] ){
+				s[0] = s[0]-1;
+				s[1] = s[1]-1;
+				count++;
+			}
+			else if( s[1] > d[1]){
+				s[1] = s[1]-1;
+				count++;
+			}
+		}
+		System.out.println(s[0]+" "+s[1]+"\n"+d[0]+" "+d[1]+" \ncount: "+count);
+	}
+	public static void choloate_distribution(){
+		int a[] = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50} , m = 7 ;
+		int n = a.length;
+		int ans = Integer.MAX_VALUE;
+		Arrays.sort(a);
+		System.out.print("\ncholoate_distribution: \n");
+		for(int i = 0; i<n-m+1;i++){
+			//System.out.println(a[i]);
+			int def = a[i+m-1] - a[i];
+			if(def < ans) ans = def;
+			
+		}
+		System.out.println(ans);
+	}
+	public static int sum_of_numbers(int n){
+		if(n < 10) return n;
+ 		return (n%10)+sum_of_numbers(n/10);
+		//return 1+sum_of_numbers(n-1) //sum of numbers
+	}
+	public static void merge_arrays(){
+		int a[] = {1,2,3,4}, m = 4, b[] = {2,5,6}, n = 3;
+		int rr[] = new int[m+n];
+		int l = 0, r = 0;
+		int count = 0;
+		
+		while(l < m && r < n){
+			if(a[l] < b[r]){
+				rr[count] = a[l];
+				count++;
+				l++;
+			}else{
+				rr[count] = b[r];
+				count++;
+				r++;
+			}
+		}
+		while(l < m) rr[count++] = a[l++];
+		while(r < n) rr[count++] = b[r++]; 
+		System.out.println(r);
+		for(int i = 0 ; i<rr.length ;i++){
+			System.out.print(rr[i]+" ");
+		}
+	}
+	public static void merging_overlap_array(){
+		int[][] a = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+		int n = a.length;
+		Arrays.sort(a, (b,c) -> b[0] - c[0]);
+		/*for(int i = 0 ;i< n; i++ ){
+			System.out.println(a[i][0]);
+		}*/
+		
+		int index = 0;
+		while(index < n){
+			int start = a[index][0];
+			int end = a[index][1];
+			
+			while(index < n-1 && end >= a[index+1][0]){
+				end = Math.max(end , a[index+1][1]);
+				index++;
+			}
+			System.out.println(start+" "+end);
+			
+			index++;
+		}
+	}
 	public static void unique_(){
 		int a[] = {1,1,2,2,3,4};
 		int n = a.length;
